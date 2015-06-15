177613ae2c1c66ded97f61d9899c908015529504
Chris Chamberlain <c.chamberlain@iinet.net.au>
2015-06-15 20:30:24 +1000

    Initial commit

---
+++ resources/lang/en/ui.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| UI Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used in buttons and other user interface elements.
+|
+*/
+
+return [
+
+    'home' => [
+        'title'         => 'Preferences',
+        'noItems'       => 'No Preferences'
+    ],
+
+    'update' => [
+        'title'         => ':name',
+        'close'         => 'Close',
+        'submit'        => 'Save'
+    ],
+
+    'theme' => [
+        'choose' => [
+            'empty' => 'No Themes'
+        ]
+    ]
+
+];
---
+++ src/PreferencesServiceProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace OxygenModule\Preferences;
+
+use Illuminate\Support\ServiceProvider;
+
+use Oxygen\Core\Blueprint\Manager;
+use Oxygen\Core\Html\Navigation\Navigation;
+use Oxygen\Preferences\Loader\ConfigLoader;
+
+class PreferencesServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+
+	public function boot() {
+		$this->package('oxygen/preferences', 'oxygen/preferences', __DIR__ . '/../resources');
+
+        $this->app[Manager::class]->loadDirectory(__DIR__ . '/../resources/blueprints');
+
+        $this->addNavigationItems();
+	}
+
+	/**
+	 * Adds items the the admin navigation.
+	 *
+	 * @return void
+	 */
+
+	public function addNavigationItems() {
+		$blueprint = $this->app[Manager::class]->get('Preferences');
+		$nav = $this->app[Navigation::class];
+
+		$nav->add($blueprint->getToolbarItem('getView'));
+	}
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        // TODO: Implement register() method.
+    }
+}
